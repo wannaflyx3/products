c4f1590d758dd0e6dbd1202e27bd5c2ed8882d1c
Pao Tsung <wannaflyx3@gmail.com>
2022-06-24 10:09:48 +0800

    add continue

--- products.py
+++ products.py
@@ -1,5 +1,20 @@
+#讀取檔案
 
 products = []
+
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #繼續，跳到下一次迴圈
+		name, price = line.strip().split(',')  #先進行strip() :捨棄換行符號\n，再進行split(',')
+		products.append([name, price])
+print(products)
+
+
+
+
+
+
 
 while True:
 	name = input('請輸入商品名稱:')
@@ -19,6 +34,7 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + p[1] + '\n')  #一定要有換行符號「\n」
